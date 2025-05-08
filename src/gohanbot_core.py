e20770d7a79c87cf4570d347688c9c7b63863a93
AhoRoshi <t7j6gtvaioo9@outlook.jp>
2025-05-08 19:25:28 +0900

    チャンネルの登録/解除機能を追加

--- src/gohanbot_core.py
+++ src/gohanbot_core.py
@@ -1,8 +1,8 @@
 import os
 import random
 import discord
-import schedule
 import json
+from discord.ext import tasks
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,11 +12,38 @@ intents = discord.Intents.all()
 bot = discord.Client(intents=intents)
 
 
-class Channel:
-    def __init__(self, config_path):
-        self.channel_path = os.path.join(
-            os.path.dirname(__file__), "..", "config", "channel_list.json"
-        )
+def toggle_channel(channel_id):
+    channel_path = os.path.join(
+        os.path.dirname(__file__), "..", "config", "channel_list.json"
+    )
+    if not os.path.exists(channel_path):
+        data = []
+    else:
+        with open(channel_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+    if channel_id in data["channels"]:
+        data["channels"].remove(channel_id)
+        with open(channel_path, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        return True
+    else:
+        data["channels"].append(channel_id)
+        with open(channel_path, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        return False
+
+
+def get_channel_list():
+    channel_path = os.path.join(
+        os.path.dirname(__file__), "..", "config", "channel_list.json"
+    )
+    if not os.path.exists(channel_path):
+        return []
+    else:
+        with open(channel_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        return data["channels"]
 
 
 def load_adjectives(file_path):
@@ -27,6 +54,19 @@ def load_adjectives(file_path):
     except FileNotFoundError:
         print(f"Error: File not found at {file_path}")
         return []
+
+
+async def send_random_message():
+    await bot.wait_until_ready()
+    channel_list = get_channel_list()
+    adjective_path = os.path.join(
+        os.path.dirname(__file__), "..", "resources", "adjective.txt"
+    )
+    adjectives = load_adjectives(adjective_path)
+    for channel_id in channel_list:
+        adjective = random.choice(adjectives)
+        channel = bot.get_channel(channel_id)
+        channel.send(f"{adjective}ごはん")
 
 
 @bot.event
@@ -40,14 +80,12 @@ async def on_message(message):
         return
     # メンションがある際にチャンネル登録の有無を切り替える
     if bot.user.mention in message.content:
-        await message.channel.send("Hello! I'm GohanBot, how can I assist you today?")
+        channel_id = message.channel.id
+        if toggle_channel(channel_id):
+            await message.channel.send("not tasty gohan...")
+        else:
+            await message.channel.send("tasty gohan!")
 
 
-if __name__ == "__main__":
-    adjective_path = os.path.join(
-        os.path.dirname(__file__), "..", "resources", "adjective.txt"
-    )
-    adjectives = load_adjectives(adjective_path)
-    adjective = random.choice(adjectives)
-
+send_random_message.start()
 bot.run(discord_token)
